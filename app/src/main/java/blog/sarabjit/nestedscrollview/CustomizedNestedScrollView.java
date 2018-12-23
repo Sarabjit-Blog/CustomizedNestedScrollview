f7eca8fc766fab10972ba138c55e1edb54f23780
Sarabjit Bagga <sarbagga@publicisgroupe.net>
2018-12-23 11:25:39 +0530

    Finding Views which are completely visible on screen in runtime

--- app/src/main/java/blog/sarabjit/nestedscrollview/CustomizedNestedScrollView.java
+++ app/src/main/java/blog/sarabjit/nestedscrollview/CustomizedNestedScrollView.java
@@ -13,7 +13,11 @@ public class CustomizedNestedScrollView extends NestedScrollView {
     private static final int DELAY = 200;
     private NestedScrollListener mNestedScrollListener;
     private boolean mIsScrolling;
+    private boolean mIsTouching;
     private Runnable mScrollingRunnable;
+    private int mOldY;
+    private int mNewY;
+    private boolean mPerformActionLater;
 
     public CustomizedNestedScrollView(Context context) {
         super(context);
@@ -32,6 +36,11 @@ public class CustomizedNestedScrollView extends NestedScrollView {
         int action = ev.getAction();
         if (action == MotionEvent.ACTION_MOVE) {
             mIsScrolling = true;
+            mIsTouching = true;
+        }
+        if (action == MotionEvent.ACTION_UP) {
+            mIsTouching = false;
+            performActionAfterTouch();
         }
         performClick();
         return super.onTouchEvent(ev);
@@ -43,6 +52,11 @@ public class CustomizedNestedScrollView extends NestedScrollView {
         return true;
     }
 
+    /**
+     * Set Listener to send callback to the parent container
+     *
+     * @param nestedScrollListener
+     */
     public void setNestedScrollListener(NestedScrollListener nestedScrollListener) {
         this.mNestedScrollListener = nestedScrollListener;
     }
@@ -50,13 +64,25 @@ public class CustomizedNestedScrollView extends NestedScrollView {
     @Override
     protected void onScrollChanged(int newX, int newY, int oldX, int oldY) {
         super.onScrollChanged(newX, newY, oldX, oldY);
-        if (Math.abs(oldY - newY) > 0) {
+        mOldY = oldY;
+        mNewY = newY;
+        checkCoordinatesAndPerformAction();
+    }
+
+    /**
+     * Determines & fire event if user has lifted the finger and scroll has stopped
+     */
+    private void checkCoordinatesAndPerformAction() {
+        if (Math.abs(mOldY - mNewY) > 0) {
             if (mScrollingRunnable != null) {
                 removeCallbacks(mScrollingRunnable);
             }
             mScrollingRunnable = new Runnable() {
                 public void run() {
-                    if (mIsScrolling && mNestedScrollListener != null) {
+                    if (mIsTouching) {
+                        mPerformActionLater = true;
+                    }
+                    if (mIsScrolling && !mIsTouching && mNestedScrollListener != null) {
                         mNestedScrollListener.onNestedScrollStopped();
                     }
                     mIsScrolling = false;
@@ -64,6 +90,23 @@ public class CustomizedNestedScrollView extends NestedScrollView {
                 }
             };
             postDelayed(mScrollingRunnable, DELAY);
+        }
+    }
+
+    /**
+     * Fire Event when Scroll has stopped but user did not lifted finger from screen.
+     */
+    private void performActionAfterTouch() {
+        if (mScrollingRunnable == null && Math.abs(mOldY - mNewY) > 0) {
+            mScrollingRunnable = new Runnable() {
+                public void run() {
+                    if (mPerformActionLater && mNestedScrollListener != null) {
+                        mNestedScrollListener.onNestedScrollStopped();
+                    }
+                    mScrollingRunnable = null;
+                }
+            };
+            post(mScrollingRunnable);
         }
     }
 
--- app/src/main/java/blog/sarabjit/nestedscrollview/MainActivity.java
+++ app/src/main/java/blog/sarabjit/nestedscrollview/MainActivity.java
@@ -1,23 +1,62 @@
 package blog.sarabjit.nestedscrollview;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements CustomizedNestedScrollView.NestedScrollListener {
     private static final String TAG = "MainActivity";
+    private Rect mRect;
+    private LinearLayout mLinearLayout;
+    private CustomizedNestedScrollView mCustomizedNestedScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CustomizedNestedScrollView customizedNestedScrollView = findViewById(R.id.nestedScrollview);
-        customizedNestedScrollView.setNestedScrollListener(this);
+        initializeViews();
+        setListener();
+    }
+
+    /**
+     * Set Listener
+     */
+    private void setListener() {
+        mCustomizedNestedScrollView.setNestedScrollListener(this);
+    }
+
+    /**
+     * Initialize Views
+     */
+    private void initializeViews() {
+        mLinearLayout = findViewById(R.id.container);
+        mCustomizedNestedScrollView = findViewById(R.id.nestedScrollview);
+        mRect = new Rect();
+        mCustomizedNestedScrollView.getHitRect(mRect);
     }
 
     @Override
     public void onNestedScrollStopped() {
         Log.d(TAG, "Nested Scroll Stopped");
-        //Write your logic here.
+        findCompletelyVisibleChildren();
+    }
+
+    /**
+     * find completely visible Positions
+     */
+    private void findCompletelyVisibleChildren() {
+        for (int i = 0; i < mLinearLayout.getChildCount(); i++) {
+            View view = mLinearLayout.getChildAt(i);
+            if (view != null) {
+                if (!view.getLocalVisibleRect(mRect) || mRect.height() < view.getHeight()) {
+                    Log.d(TAG, "View " + view.getTag() + " is Partially Visible");
+                } else {
+                    Log.d(TAG, "View " + view.getTag() + " is Completely Visible");
+                }
+            }
+        }
     }
 }
